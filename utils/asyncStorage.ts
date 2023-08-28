b6c82a4b3b9bb702071318113c86501c68a5e8f8
sachinelavarasan <sachinelavarasan321@gmail.com>
2023-08-28 19:21:05 +0530

    searched word screen

--- utils/asyncStorage.ts
+++ utils/asyncStorage.ts
@@ -16,3 +16,25 @@ export const getData = async (key: string) => {
     alert(message);
   }
 };
+
+export const storeSearchData = async (keyword: any) => {
+  try {
+    const value = await AsyncStorage.getItem("@searchWords");
+    const searchedValue = value ? JSON.parse(value) : [];
+    searchedValue.push(keyword);
+    const duplicateRemoved = new Set(searchedValue);
+    let originalArray = Array.from(duplicateRemoved);
+    await AsyncStorage.setItem("@searchWords", JSON.stringify(originalArray));
+  } catch ({ message }: any) {
+    alert(message);
+  }
+};
+
+export const getSearchData = async () => {
+  try {
+    const value = await AsyncStorage.getItem("@searchWords");
+    return value ? JSON.parse(value) : [];
+  } catch ({ message }: any) {
+    alert(message);
+  }
+};
